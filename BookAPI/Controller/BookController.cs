ecae3831b322e90317b289497ce9ddffa708ff99
nguyen <kanicepakulate@gmail.com>
2020-12-23 10:33:35 +0700

    thi

---
+++ BookAPI/Controller/BookController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookBAL.Interface;
+
+namespace BookAPI.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly IBooks
+    }
+}
